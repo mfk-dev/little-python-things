d3f793d20dcf1df909aaa190a6a2ea5a6f14517b
MFK <114757075+mfk-dev@users.noreply.github.com>
2025-08-20 16:17:43 +0300

    Update simple_orbit_calculator.py

--- simple_orbit_calculator.py
+++ simple_orbit_calculator.py
@@ -1,7 +1,8 @@
 import math
 
-a = float(input("Distance from the Planet in AU:"))
-M = float(input("Mass of the star (Sun = 1): "))
-T = math.sqrt(a**3 / M)
+a = float(input("Distance from the Planet in AU:")) # Distance of planet to star (in Astronomical Units)
+M = float(input("Mass of the star (Sun = 1): ")) # Mass of the star (Assuming Sun=1)
+T = math.sqrt(a**3 / M)   # Kepler's Law
 
 print("Orbital Period is:", T, "Years")
+
