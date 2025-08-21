354b821e8d111590f74ff5cb1dc36cc2beda3457
MFK <114757075+mfk-dev@users.noreply.github.com>
2025-08-21 10:32:17 +0300

    Update simple_orbital_period_calculator.py

--- simple_orbital_period_calculator.py
+++ simple_orbital_period_calculator.py
@@ -1,3 +1,5 @@
+# This is an very simple Orbital Period calculator.
+
 import math
 
 a = float(input("Distance from the Planet in AU:")) # Distance of planet to star (in Astronomical Units)
@@ -5,4 +7,5 @@ M = float(input("Mass of the star (Sun = 1): ")) # Mass of the star (Assuming Su
 T = math.sqrt(a**3 / M)   # Kepler's Law
 
 print("Orbital Period is:", T, "Years")
+
 
