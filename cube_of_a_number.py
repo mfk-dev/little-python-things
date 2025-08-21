48c47f7df82ac423924987dfe9a8e28134b54627
MFK <114757075+mfk-dev@users.noreply.github.com>
2025-08-21 10:31:38 +0300

    Update cube_of_a_number.py

--- cube_of_a_number.py
+++ cube_of_a_number.py
@@ -1,6 +1,9 @@
+This is a thing that calculates the cube of a number u say.
+
 def cube(a):
     return a**3
 
 number = int(input("Tell me the number please: "))
 print(f"Cube of {number} is: {cube(number)}")
+
 
